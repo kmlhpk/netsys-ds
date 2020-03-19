43ef16e2de9719491e2aa23cec32b334a2ad49bc
kmlhpk <44464908+kmlhpk@users.noreply.github.com>
2020-03-19 21:30:28 +0000

    haha

--- back.py
+++ back.py
@@ -3,15 +3,14 @@ import datetime # TODO Is this necessary
 import postcodes_io_api
 
 
-
-
-
 print("Backend Server Started")
+menu = ["Pizza","Curry","Burger","Kebab","Chips"]
 orders = []
 
 
 apiFail = False
 validPostcode = False
+
 try:
     api = postcodes_io_api.Api()
     validPostcode = api.is_postcode_valid("nn57nw")
